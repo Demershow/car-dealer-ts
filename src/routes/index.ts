0668aecc838b13f1fb1802bc2003c44fa8bafcbe
demershow <demersontorres520@gmail.com>
2023-07-01 23:54:05 -0300

    :fire: removing useless test file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { userRoutes } from "./userRoutes";
 
 const routes = Router()
 
 routes.use('/users', userRoutes)
+routes.get('/', (req: Request, res: Response) => {
+  res.send('API is Running')
+})
 
 export default routes
